36e10d3f4cbb3733447416771a5947855696eb14
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-05-30 09:15:42 +0700

    add table products

---
+++ database/migrations/2023_05_30_021221_create_products.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('business_id');
+            $table->string('name');
+            $table->double('price')->nullable();
+            $table->text('description')->nullable();
+            $table->string('product_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
