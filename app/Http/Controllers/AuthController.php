56aba2d0922684926c22cacc34ccd40a070b671a
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-06-12 13:01:37 +0700

    add template

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login', [
+            'title' => 'Login'
+        ]);
+    }
+    public function authenticate(Request $request)
+    {
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+        $credentials = $request->validate($rules);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+        return back()->with('loginError', 'Login gagal!');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
+}
