da294817cd56878d26f5f72111a11c66a6b888ac
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-06-12 22:32:29 +0700

    add orders and order_details

---
+++ database/migrations/2023_06_12_152552_create_orders.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('business_id');
+            $table->uuid('payment_id');
+            $table->uuid('user_id');
+            $table->string('status', 100);
+            $table->integer('total_price');
+            $table->timestamps();
+
+            $table->foreign('business_id')->references('id')->on('businesses');
+            $table->foreign('payment_id')->references('id')->on('payments');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2023_06_12_152859_create_order_details.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('product_id');
+            $table->uuid('order_id');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('total_price');
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
