a6294984006fe5424a84ff9b10c4a586286e521d
satriadinatati <satriadinata.ratnanto@ti.ukdw.ac.id>
2023-06-12 13:13:39 +0700

    login token

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/login', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
